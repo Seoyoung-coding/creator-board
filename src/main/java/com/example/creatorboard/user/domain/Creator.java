b47911313c6925330bd6f53a2ec9de5c192e2088
seoyoung <seoyoung.project@gmail.com>
2026-04-13 02:03:58 +0900

    Creator domain

--- src/main/java/com/example/creatorboard/user/domain/Creator.java
+++ src/main/java/com/example/creatorboard/user/domain/Creator.java
@@ -1,4 +1,29 @@
 package com.example.creatorboard.user.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "creators")
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //외부 객체생성 방지
+@AllArgsConstructor
+@Builder
+
 public class Creator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //은행이름
+    @Column(length = 100)
+    private String bank_name;
+
+    //예금주명
+    @Column(length = 100)
+    private String name;
 }
